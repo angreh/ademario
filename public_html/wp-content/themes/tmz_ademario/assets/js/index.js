ac1ed54340f190c3bbcd4761fcea0166c2e8b31c
André Pinto <Andr Pinto>
2016-07-01 10:13:29 -0300

    Renposividade do menu

--- public_html/wp-content/themes/tmz_ademario/assets/js/index.js
+++ public_html/wp-content/themes/tmz_ademario/assets/js/index.js
@@ -20,4 +20,14 @@ $(function()
         });
     }
 
+    $('#header_mobile_menu').on('click', function(){
+        $("#mobile_bg").show(0);
+        $("#mobile_menu").css({'margin-right':'0px'});
+    });
+
+    $('#mobile_bg').on('click',function(){
+        $("#mobile_bg").slideUp(300);
+        $("#mobile_menu").css({'margin-right':'-300px'});
+    });
+
 });
